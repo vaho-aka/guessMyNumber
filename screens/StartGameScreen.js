72abeab4c57f0e1080929e4fcf696e40d052e5d8
vahoaka <irnarakotoarinirina@gmail.com>
2024-03-10 16:49:17 +0300

    Styling StartGameScreen inputContainer.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import React from 'react';
+import PrimaryButton from '../components/PrimaryButton.js';
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <Text>StartGameScreen</Text>
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 };
 
 export default StartGameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: '#72063c',
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 6,
+  },
+});
